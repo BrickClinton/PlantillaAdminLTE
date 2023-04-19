048dab64cb1723509dbd9d602507d7d6e8d8085c
Brick Clinton <1321063@senati.pe>
2023-04-18 19:17:57 -0500

    loadweb mejorado

--- dist/js/loadweb.js
+++ dist/js/loadweb.js
@@ -1,5 +1,21 @@
 function getParam(param){
   var result = window.location.search.match(new RegExp("(\\?|&)" + param + "(\\[\\])?=([^&]*)"));
-  //console.log(result);
   return result ? result[3] : false
+}
+
+function getParamPro(param) {
+  // Recupera el parámetro de consulta de la URL
+  var result = window.location.search.match(new RegExp("(\\?|&)" + param + "(\\[\\])?=([^&]*)"));
+  var value = result ? result[3] : false;
+  
+  // Oculta la URL utilizando la función history.replaceState()
+  let pathname = (window.location.pathname).split("/");
+  var newUrl = window.location.protocol + "//" + window.location.host + "/" + pathname[1] + "/#";
+  history.replaceState(null, '', newUrl);
+
+  if(value !== false){
+    sessionStorage.setItem('lastVisitedURL', value);
+  }
+  
+  return value;
 }
